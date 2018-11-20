465ce52ef59774fdcb704c92b2ad42c460950231
iibanezCode <40260420+iibanezCode@users.noreply.github.com>
2018-11-20 20:56:31 +0100

    InicioFragment

---
+++ app/src/main/java/com/baren/almi/almibarenandroid/adapter/ProductosInicioAdapter.java
@@ -0,0 +1,148 @@
+package com.baren.almi.almibarenandroid.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.baren.almi.almibarenandroid.Productos;
+import com.baren.almi.almibarenandroid.singleton.CardViewSingleton;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductosInicioAdapter extends BaseAdapter {
+    private Context context;
+    private RequestQueue requestQueue;
+    private JsonRequest jsArrayRequest;
+    private String URL_BASE = "http://192.168.6.161/almibarenBackend/products/";
+
+
+    public List<Productos> getListPopulares() {
+        Log.d("JON","ESTOY COGIENDO LA LISTA");
+        return list.get(0);
+    }
+
+    public List<Productos> getListRecomendados() {
+        return list.get(1);
+    }
+
+    public List<Productos> getListValorados() {
+        return list.get(2);
+    }
+
+    public List<Productos> getListOfertas() {
+        return list.get(3);
+    }
+
+    public boolean isListNull() {
+        return (list==null);
+    }
+
+    private List<List<Productos>> list;
+
+    public ProductosInicioAdapter(Context context) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, URL_BASE, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                list = parseJson(response);
+                notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        CardViewSingleton.getInstance(context).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private List<List<Productos>> parseJson(JSONObject jsonObject) {
+        List<List<Productos>> list = new ArrayList<List<Productos>>();
+        List<Productos> listPopulares = new ArrayList<>();
+        List<Productos> listRecomendados = new ArrayList<>();
+        List<Productos> listValorados = new ArrayList<>();
+        List<Productos> listOfertas = new ArrayList<>();
+        try {
+            JSONArray jsonArrayPopulares = jsonObject.getJSONArray("populares");
+            JSONArray jsonArrayRecomendados = jsonObject.getJSONArray("recomendados");
+            JSONArray jsonArrayValorados = jsonObject.getJSONArray("valorados");
+            JSONArray jsonArrayOfertas = jsonObject.getJSONArray("ofertas");
+
+
+            for (int i = 0; i < jsonArrayPopulares.length(); i++) {
+
+                JSONObject jsonObject1 = jsonArrayPopulares.getJSONObject(i);
+                Productos producto = new Productos(jsonObject1.getString("url"), jsonObject1.getString("id"), jsonObject1.getString("nombre"), jsonObject1.getString("precio"), jsonObject1.getString("descuento"));
+                listPopulares.add(producto);
+            }
+
+            for (int i = 0; i < jsonArrayRecomendados.length(); i++) {
+
+                JSONObject jsonObject1 = jsonArrayRecomendados.getJSONObject(i);
+                Productos producto = new Productos(jsonObject1.getString("url"), jsonObject1.getString("id"), jsonObject1.getString("nombre"), jsonObject1.getString("precio"), jsonObject1.getString("descuento"));
+                listRecomendados.add(producto);
+
+            }
+            for (int i = 0; i < jsonArrayValorados.length(); i++) {
+                JSONObject jsonObject1 = jsonArrayValorados.getJSONObject(i);
+                Productos producto = new Productos(jsonObject1.getString("url"), jsonObject1.getString("id"), jsonObject1.getString("nombre"), jsonObject1.getString("precio"), jsonObject1.getString("descuento"));
+                listValorados.add(producto);
+
+            }
+            for (int i = 0; i < jsonArrayOfertas.length(); i++) {
+
+                JSONObject jsonObject1 = jsonArrayOfertas.getJSONObject(i);
+                Productos producto = new Productos(jsonObject1.getString("url"), jsonObject1.getString("id"), jsonObject1.getString("nombre"), jsonObject1.getString("precio"), jsonObject1.getString("descuento"));
+                listOfertas.add(producto);
+
+            }
+            list.add(listPopulares);
+            list.add(listRecomendados);
+            list.add(listValorados);
+            list.add(listOfertas);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+
+    }
+
+    @Override
+    public int getCount() {
+
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View cardItemview = convertView;
+
+        return null;
+    }
+}
--- app/src/main/java/com/baren/almi/almibarenandroid/adapter/recycler/PopularesRVAdapter.java
+++ app/src/main/java/com/baren/almi/almibarenandroid/adapter/recycler/PopularesRVAdapter.java
@@ -6,11 +6,19 @@ import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.baren.almi.almibarenandroid.Productos;
 import com.baren.almi.almibarenandroid.R;
+import com.bumptech.glide.Glide;
 
-public class PopularesRVAdapter extends RecyclerView.Adapter<PopularesRVAdapter.MyViewHolder>{
+import java.util.List;
+
+public class PopularesRVAdapter extends RecyclerView.Adapter<PopularesRVAdapter.MyViewHolder> {
     private Context context;
+    private List<Productos> list;
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public CardView mCardView;
 
@@ -20,23 +28,31 @@ public class PopularesRVAdapter extends RecyclerView.Adapter<PopularesRVAdapter.
         }
     }
 
-    public PopularesRVAdapter(Context context){this.context = context;}
+    public PopularesRVAdapter(Context context, List<Productos> list) {
+        this.context = context;
+        this.list = list;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        CardView c = (CardView) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cardview_example,viewGroup,false);
-        return null;
+        CardView cardView = (CardView) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cardview_example, viewGroup, false);
+        MyViewHolder myViewHolder = new MyViewHolder(cardView);
+        return myViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        ((TextView) myViewHolder.mCardView.findViewById(R.id.tvProducto)).setText(list.get(i).getNombre());
+        Glide.with(context).load(list.get(i).getUrl()).into((ImageView) myViewHolder.mCardView.findViewById(R.id.ivProducto));
+        ((TextView) myViewHolder.mCardView.findViewById(R.id.tvPrecioProducto)).setText(list.get(i).getPrecio());
+        ((TextView) myViewHolder.mCardView.findViewById(R.id.tvDtoProducto)).setText(list.get(i).getDescuento());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
     }
 
 
